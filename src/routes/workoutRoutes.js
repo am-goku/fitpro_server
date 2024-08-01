aa08b8528aa1b1578dd5c8764f467b4f26490a69
GANLAXMINE <ganlaxmineprivatelimited@gmail.com>
2024-08-01 16:28:16 +0530

    bug fixed in workout feature

--- src/routes/workoutRoutes.js
+++ src/routes/workoutRoutes.js
@@ -2,21 +2,22 @@ const express = require('express');
 const router = express.Router();
 const { createUserWorkoutController, readUserWorkoutController, updateExerciseCompletionController } = require('../controllers/workoutController'); // Adjust the path if needed
 const { createWorkoutController, fetchWorkoutController, deleteWorkoutController } = require('../controllers/workoutController'); // Adjust the path if needed
+const { adminProtect, userProtect } = require('../middleware/authMiddleware');
 
 // Route to create a new workout
-router.post('/', createWorkoutController);
+router.post('/', adminProtect, createWorkoutController);
 
 // Route to fetch a specific workout by ID or all workouts
-router.get('/', fetchWorkoutController);
+router.get('/', userProtect, fetchWorkoutController);
 
 // Route to delete a workout by ID
-router.delete('/:workoutID', deleteWorkoutController);
+router.delete('/:workoutID', adminProtect, deleteWorkoutController);
 
-router.post('/user-workouts/:workoutID', createUserWorkoutController);
+router.post('/user-workouts/:workoutID', userProtect, createUserWorkoutController);
 
-router.get('/user-workouts', readUserWorkoutController);
+router.get('/user-workouts', userProtect, readUserWorkoutController);
 
-router.patch('/user-workouts/:workoutID/exercises/:exerciseID', updateExerciseCompletionController);
+router.patch('/user-workouts/:workoutID/exercises/:exerciseID', userProtect, updateExerciseCompletionController);
 
 
 module.exports = router;
